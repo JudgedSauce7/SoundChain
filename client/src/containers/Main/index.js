ad442e315cdfd8bb3e5a5da0df114cd3e8d2ecb2
Pooja Pant <pooja.pant0602@gmail.com>
2020-11-24 23:58:09 +0530

    local changes

--- client/src/containers/Main/index.js
+++ client/src/containers/Main/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import {Row, Col, Card} from "antd"
+import {Row, Col, Card, Avatar, Button} from "antd"
 import {FireOutlined} from "@ant-design/icons"
 import Profile from "./components/Profile/index"
 import Media from "./components/Media/index"
 import Explore from "./components/Explore/index"
+import EmojiSymbolsIcon from '@material-ui/icons/EmojiSymbols';
 import "./style.css"
-
 
 export default class index extends Component {
     
@@ -15,10 +15,35 @@ export default class index extends Component {
             <div className="mainWindow">
                 {currentActiveLink === "home" ? 
                 <Row> 
-                    <Col span={16}>               
-                    <div style={{marginTop: "70px", marginLeft: "-70px"}}>
+                    <Col span={16}> 
+                    <Row style={{display: "flex", alignItems: "center", marginTop: 40}}>
+                        <Col span={8}>
+                        <div style={{marginLeft: 20, boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius: 20}}>
+                            <EmojiSymbolsIcon style={{width: 200, height: 200, color: "rgba(108, 31, 109, 0.89)", zIndex: 1}}/>  
+                        </div>
+                        </Col>
+                        <Col span={16}>
+                            <div className="text">
+                                <p>Share your music and earn 100% of your tips and sales</p>
+                            </div>
+                        </Col>
+                     </Row> 
+
+                <Row>
+                    <Col span={24}>
+                    {/* <Card className="card"></Card> */}
+                    </Col>
+                </Row>    
+
+                <Row>
+                    <Col span={24}>
+                        
+                    </Col>
+                </Row>         
                     
-                </div>
+                    
+                    
+                
                 </Col>
                 <Col span={8} style={{marginTop: "100px"}}>
                 <span style={{display: "flex", alignItems: "center"}}>
